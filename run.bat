ef81850408b958e5652d82b56d1ae04cb07ed433
NBB1996 <bayerniclasbardo@gmail.com>
2025-07-19 18:34:36 +0200

    Optimierung der run.bat Datei

--- run.bat
+++ run.bat
@@ -1,10 +1,25 @@
 @echo off
+echo [INFO] Starte Reisequiz...
+
+REM Virtuelle Umgebung erstellen, falls nicht vorhanden
 if not exist venv (
-  python -m venv venv
-  venv\Scripts\activate
-  pip install --upgrade pip
-  pip install -r requirements.txt
+    echo [INFO] Erstelle virtuelle Umgebung...
+    python -m venv venv
 )
-venv\Scripts\activate
+
+REM Aktivieren der virtuellen Umgebung
+call venv\Scripts\activate.bat
+
+REM Abhängigkeiten installieren
+echo [INFO] Installiere Python-Abhängigkeiten...
+pip install --upgrade pip
+pip install -r requirements.txt
+
+REM Anwendung starten
+echo [INFO] Starte Anwendung...
 python run.py
+
+REM Fenster offen halten
+echo.
+echo [INFO] Anwendung wurde beendet. Zum Schließen eine Taste drücken...
 pause
